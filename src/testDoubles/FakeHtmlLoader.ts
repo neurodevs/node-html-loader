618620f009e1f259c7f79948e9c88da7fff5a1db
eric-yates <hello@ericthecurious.com>
2024-10-08 11:08:24 -0600

    patch: create attribute to track constructor calls

--- src/testDoubles/FakeHtmlLoader.ts
+++ src/testDoubles/FakeHtmlLoader.ts
@@ -2,8 +2,13 @@ import { generateId } from '@sprucelabs/test-utils'
 import { HtmlLoader } from '../HtmlLoader'
 
 export default class FakeHtmlLoader implements HtmlLoader {
+    public static numCallsToConstructor = 0
     public static callsToLoad: string[] = []
     private static fakedResponse: string = generateId()
+
+    public constructor() {
+        FakeHtmlLoader.numCallsToConstructor++
+    }
 
     public static setFakedResponse(fakeResponse: string) {
         FakeHtmlLoader.fakedResponse = fakeResponse
